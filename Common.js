8f0270621d9c88d7c9a3cbcefb0475e915b2c41e
Loren-Wang <wangliang_workjob@163.com>
2019-09-20 16:45:37 +0800

    Update Common.js

--- Common.js
+++ Common.js
@@ -18,27 +18,32 @@ function jstlwGetUrlParams(key) {
     if (jstlwParamsIsString(key)) {
         //地址转码
         let url = decodeURI(document.URL);
-        //读取参数部分
-        let params = url.substring(url.indexOf("?"), url.length);
-        //是否有参数
-        if (params.length > 0) {
-            params = params.substring(1, params.length)
+        let reg = new RegExp("(^|&)"+ key +"=([^&]*)(&|$)");
+        let arg = window.location.search.substr(1).match(reg);
+        if(arg!=null){
+            return  unescape(r[2]);
         }
-        //判断是否是多参数
-        let index = params.indexOf("&");
-        if (index >= 0) {
-            params.split("&").forEach(item => {
-                index = item.indexOf("=");
-                if (index >= 0 && key === item.substring(0, index)) {
-                    return item.substring(index + 1, item.length)
-                }
-            })
-        } else {
-            index = params.indexOf("=");
-            if (index >= 0 && key === params.substring(0, index)) {
-                return params.substring(index + 1, params.length)
-            }
-        }
+//         //读取参数部分
+//         let params = url.substring(url.indexOf("?"), url.length);
+//         //是否有参数
+//         if (params.length > 0) {
+//             params = params.substring(1, params.length)
+//         }
+//         //判断是否是多参数
+//         let index = params.indexOf("&");
+//         if (index >= 0) {
+//             params.split("&").forEach(item => {
+//                 index = item.indexOf("=");
+//                 if (index >= 0 && key === item.substring(0, index)) {
+//                     return item.substring(index + 1, item.length)
+//                 }
+//             })
+//         } else {
+//             index = params.indexOf("=");
+//             if (index >= 0 && key === params.substring(0, index)) {
+//                 return params.substring(index + 1, params.length)
+//             }
+//         }
     }
     return key
 }
